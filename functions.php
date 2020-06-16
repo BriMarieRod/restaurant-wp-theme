3900f45b94e7c9b8dd66bb6f04d9d4f3f99b645c
Brianna Marie <briannarodriguez@gmail.com>
2020-06-15 23:49:01 -0500

    Change website fonts

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function restaurant_scripts() {
 	wp_enqueue_style( 'bootstrap-css', get_theme_file_uri('/css/bootstrap.min.css') );
 	wp_enqueue_style( 'custom-css', get_theme_file_uri('/css/custom.css') );
 	wp_enqueue_style( 'header-css', get_theme_file_uri('/css/header.css') );
+	wp_enqueue_style( 'fonts', get_theme_file_uri('/css/fonts.css') );
 	wp_enqueue_style( 'font-awesome', get_theme_file_uri('/fontawesome/css/all.css') );
 
 	wp_enqueue_script( 'jquery', get_theme_file_uri('/js/jquery-3.5.1.slim.min.js') );
